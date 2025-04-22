f6126343970814834e2be188f4b9ee1ecd761e07
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-04-22 00:06:26 -0600

    Cambios en vista de mantenimientos del vehiculo

--- src/views/admin/cars/AdminCarDetails.tsx
+++ src/views/admin/cars/AdminCarDetails.tsx
@@ -8,10 +8,9 @@ export default function AdminCarDetails() {
     const params = useParams(); // Obtiene el ID de la URL
     const carId = +params.carId!;
 
-    // Consulta para obtener los detalles del vehículo
-    const { data: carData, isLoading: isCarLoading, isError: isCarError } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryKey: ['car', carId],
-        queryFn: () => getCarById(carId),
+        queryFn: () => getCarById(carId), // Llama a la API para obtener los detalles
     });
 
     // Consulta para obtener los mantenimientos del vehículo
@@ -20,68 +19,96 @@ export default function AdminCarDetails() {
         queryFn: () => getMaintenancesByVehicleId(carId),
     });
 
-    if (isCarLoading || isMaintenanceLoading) return <p className="text-center text-lg">Cargando...</p>;
-    if (isCarError || isMaintenanceError) return <p className="text-center text-lg text-red-500">Error al cargar los detalles.</p>;
+    if (isLoading) return <p className="text-center text-lg">Cargando...</p>;
+    if (isError) return <p className="text-center text-lg text-red-500">Error al cargar los detalles del auto.</p>;
 
-    if (carData) {
+    if (data) {
         return (
-            <div className="max-w-4xl mx-auto p-6">
-                <h1 className="text-3xl font-bold text-gray-800 mb-6">Detalles del Auto</h1>
-                <div className="bg-white shadow-md rounded-lg overflow-hidden border border-gray-300">
-                    {/* Imagen del auto */}
-                    <div className="bg-sky-100 p-6 flex justify-center">
-                        <img
-                            src={`/autos/${carData.imagen}.png`}
-                            alt={carData.modelo}
-                            className="max-h-60 object-contain"
-                        />
-                    </div>
-
-                    {/* Información del auto */}
-                    <div className="p-6">
-                        <div className="flex justify-between">
-                            <h2 className="text-2xl font-bold text-gray-800 mb-4">{carData.modelo}</h2>
-                            <p className="text-3xl font-bold text-sky-800 mb-4">
-                                <span className="text-lg text-gray-800 font-normal px-2">Precio por día: </span>
-                                {formatCurrency(carData.precio_por_dia)}
-                            </p>
+            <>
+                <h1 className="text-3xl font-bold text-gray-800 mx-10 mt-10">Detalles del Auto</h1>
+                <div className="m-10 flex gap-6 bg-white shadow-md rounded-lg overflow-hidden border border-gray-300">
+                    <div className="">
+                        {/* Imagen del auto */}
+                        <div className="bg-sky-100 p-6 flex justify-center">
+                            <img
+                                src={`/autos/${data.imagen}.png`}
+                                alt={data.modelo}
+                                className="max-h-60 object-contain"
+                            />
                         </div>
-                        <div className="flex gap-6">
-                            <div className="grid grid-cols-2 bg-green-50 border border-green-300 rounded-md p-4">
-                                <div className="space-y-2">
-                                    <p className="text-gray-600">
-                                        <strong className="text-green-600">Marca:</strong> {carData.marca}
-                                    </p>
-                                    <p className="text-gray-600">
-                                        <strong className="text-green-600">Año:</strong> {carData.anio}
-                                    </p>
-                                    <p className="text-gray-600">
-                                        <strong className="text-green-600">Color:</strong> {carData.color}
-                                    </p>
-                                    <p className="text-gray-600">
-                                        <strong className="text-green-600">Tipo:</strong> {carData.tipo}
-                                    </p>
+
+                        {/* Información del auto */}
+                        <div className="p-6">
+                            <div className="flex justify-between">
+                                <h2 className="text-2xl font-bold text-gray-800 mb-4">{data.modelo}</h2>
+                                <p 
+                                    className="text-3xl font-bold text-sky-800 mb-4"
+                                >
+                                    <span className="text-lg text-gray-800 font-normal px-2">Precio por dia: </span>
+                                    {formatCurrency(data.precio_por_dia)}
+                                </p>
+                            </div>
+                            <div className="flex gap-6">
+                                <div className="grid grid-cols-2 bg-green-50 border border-green-300 rounded-md p-4">
+                                    <div className="space-y-2">
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Marca:</strong> {data.marca}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Año:</strong> {data.anio}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Color:</strong> {data.color}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Tipo:</strong> {data.tipo}
+                                        </p>
+                                    </div>
+                                    <div className="space-y-2">
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Transmisión:</strong> {data.transmision}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Puertas:</strong> {data.puertas}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Asientos:</strong> {data.asientos}
+                                        </p>
+                                        <p className="text-gray-600">
+                                            <strong className="text-green-600">Clima:</strong> {data.clima ? 'Con Clima' : 'Sin Clima'}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div className="space-y-2">
+                                <div className="flex-1 p-4 space-y-2 bg-sky-50 border border-sky-300 rounded-lg">
                                     <p className="text-gray-600">
-                                        <strong className="text-green-600">Transmisión:</strong> {carData.transmision}
+                                        <strong className="text-sky-600">Tipo:</strong> {data.seguro.tipo}
                                     </p>
                                     <p className="text-gray-600">
-                                        <strong className="text-green-600">Puertas:</strong> {carData.puertas}
+                                        <strong className="text-sky-600">Cobertura:</strong> {data.seguro.cobertura}
                                     </p>
                                     <p className="text-gray-600">
-                                        <strong className="text-green-600">Asientos:</strong> {carData.asientos}
+                                        <strong className="text-sky-600">Descripcion:</strong> {data.seguro.descripcion}
                                     </p>
                                     <p className="text-gray-600">
-                                        <strong className="text-green-600">Clima:</strong> {carData.clima ? 'Con Clima' : 'Sin Clima'}
+                                        <strong className="text-sky-600">Precio:</strong> {formatCurrency(data.seguro.precio)}
                                     </p>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
+                        {/* Información adicional */}
+                        <div className="bg-gray-200 p-6">
+                            <h3 className="text-xl font-semibold text-gray-800 mb-4">Información Adicional</h3>
+                            <p className="text-gray-600 mb-2">
+                                <strong>Creado en:</strong> {new Date(data.createdAt).toLocaleDateString()}
+                            </p>
+                            <p className="text-gray-600">
+                                <strong>Actualizado en:</strong> {new Date(data.updatedAt).toLocaleDateString()}
+                            </p>
+                        </div>
+                    </div>
                     {/* Información de mantenimientos */}
-                    <div className="bg-gray-100 p-6">
+                    <div className=" p-6">
                         <h3 className="text-xl font-semibold text-gray-800 mb-4">Mantenimientos</h3>
                         {maintenanceData?.mantenimientos?.length > 0 ? (
                             <table className="min-w-full divide-y divide-gray-300">
@@ -110,19 +137,9 @@ export default function AdminCarDetails() {
                             <p className="text-gray-600">No hay mantenimientos registrados para este vehículo.</p>
                         )}
                     </div>
-
-                    {/* Información adicional */}
-                    <div className="bg-gray-200 p-6">
-                        <h3 className="text-xl font-semibold text-gray-800 mb-4">Información Adicional</h3>
-                        <p className="text-gray-600 mb-2">
-                            <strong>Creado en:</strong> {new Date(carData.createdAt).toLocaleDateString()}
-                        </p>
-                        <p className="text-gray-600">
-                            <strong>Actualizado en:</strong> {new Date(carData.updatedAt).toLocaleDateString()}
-                        </p>
-                    </div>
                 </div>
-            </div>
+            </>
+            
         );
     }
 
