4f6becf641f808127f1684d82092cbc4c4575be8
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-05-01 13:33:55 -0600

    Desarrollo de funcion createBranch con axios en BranchesAPI

--- src/api/BranchesAPI.ts
+++ src/api/BranchesAPI.ts
@@ -1,6 +1,18 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { adminBranchSchema } from "../types";
+import { adminBranchSchema, BranchFormData } from "../types";
+
+
+export async function createBranch(formData: BranchFormData) {
+    try {
+        const { data } = await api.post('/branches', formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
 
 export async function getAllBranches() {
     try {
