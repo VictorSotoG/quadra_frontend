a7518eebbbbea8f52bcfa0084a5c6925eff38b51
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-05-06 18:23:27 -0600

    Creacion de nuevo metodo para obtener seguro por su Id, Creacion de componente AdminEditInsuranceForm para mostrar el formulario para editar el seguro, y cambio en AdminInsurancesEditView para renderizar el formulario

--- src/api/InsurancesAPI.ts
+++ src/api/InsurancesAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { adminInsurancesSchema, InsuranceFormData } from "../types";
+import { adminInsurancesSchema, Insurance, InsuranceFormData, insuranceSchema } from "../types";
 
 
 export async function createInsurance(formData: InsuranceFormData) {
@@ -19,6 +19,20 @@ export async function getAllInsurances() {
     try {
         const { data } = await api('/insurances');
         const response = adminInsurancesSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getInsuranceById(id: Insurance['id']) {
+    try {
+        const { data } = await api(`/insurances/${id}`)
+        const response = insuranceSchema.safeParse(data)
         if (response.success) {
             return response.data
         }
--- src/views/admin/insurances/AdminInsurancesEditView.tsx
+++ src/views/admin/insurances/AdminInsurancesEditView.tsx
@@ -1,125 +1,22 @@
-import { useForm } from "react-hook-form"
-import { InsuranceFormData } from "../../../types"
-import ErrorMessage from "../../../components/ErrorMessage"
+
+import { Navigate, useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import { getInsuranceById } from "../../../api/InsurancesAPI";
+import AdminEditInsuranceForm from "../../../components/Insurances/AdminEditInsuranceForm";
 
 export default function AdminInsuranceEditView() {
 
-    const initialValues : InsuranceFormData = {
-        tipo: '',
-        cobertura: '',
-        precio: 0,
-        descripcion: ''
-    }
+    const params  = useParams();
+    const insuranceId = +params.insuranceId!
 
-    const { register, handleSubmit, formState: {errors} } = useForm({ defaultValues: initialValues})
 
-    const handleRegister = (formData: InsuranceFormData) => {
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['editInsurance', insuranceId],
+        queryFn: () => getInsuranceById(insuranceId)
+    })
 
-    }
-
-    return (
-        <>
-            <div className=" max-w-2xl mx-auto p-10">
-                <h2 className="text-2xl font-bold mb-8 text-gray-700">Editar Seguro</h2>
-                <div className="bg-white p-6 rounded-lg">
-                    <form 
-                        className="w-full"
-                        noValidate
-                        onSubmit={handleSubmit(handleRegister)}
-                    >
-
-                        <div className="mb-4">
-                            <label 
-                                className="block text-gray-700 mb-2" 
-                                htmlFor="tipo"
-                            >Tipo</label>
-
-                            <input
-                                id="tipo"
-                                type="text"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
-                                placeholder="Ej: Basico o Premium"
-                                {...register("tipo", {
-                                    required: "El Tipo de Seguro es obligatorio",
-                                })}
-                            />
-                            {errors.tipo && (
-                                <ErrorMessage>{errors.tipo.message}</ErrorMessage>
-                            )}
-                        </div>
-
-                        <div className="mb-4">
-                            <label 
-                                className="block text-gray-700 mb-2" 
-                                htmlFor="cobertura"
-                            >Cobertura</label>
-
-                            <input
-                                id="cobertura"
-                                type="text"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
-                                placeholder="Ej: Total o de Terceros"
-                                {...register("cobertura", {
-                                    required: "La Cobertura es obligatoria"
-                                })}
-                            />
-                            {errors.cobertura && (
-                                <ErrorMessage>{errors.cobertura.message}</ErrorMessage>
-                            )}
-                        </div>
-
-                        <div className="mb-4">
-                            <label 
-                                className="block text-gray-700 mb-2" 
-                                htmlFor="precio"
-                            >Precio</label>
-
-                            <input
-                                id="precio"
-                                type="number"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
-                                placeholder="Ej: 4500"
-                                {...register("precio", {
-                                    required: "El precio es obligatorio"
-                                })}
-                            />
-                            {errors.precio && (
-                                <ErrorMessage>{errors.precio.message}</ErrorMessage>
-                            )}
-                        </div>
-
-                        <div className="mb-4">
-                            <label 
-                                className="block text-gray-700 mb-2" 
-                                htmlFor="descripcion"
-                            >Descripcion</label>
-
-                            <textarea
-                                id="descripcion"
-                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
-                                {...register("descripcion", {
-                                    required: "La descripcion es obligatoria"
-                                })}
-                            />
-                            {errors.descripcion && (
-                                <ErrorMessage>{errors.descripcion.message}</ErrorMessage>
-                            )}
-                        </div>
-
-                        <button 
-                            type="submit"
-                            className="relative group w-full text-white font-bold py-3 rounded-lg overflow-hidden mt-6"
-                        >
-                            {/* Capa de fondo inicial */}
-                            <div className="absolute inset-0 bg-gradient-to-l from-blue-400 to-blue-500 transition-opacity duration-1000 group-hover:opacity-0"></div>
-                            {/* Capa de fondo para el hover */}
-                            <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-500 opacity-0 transition-opacity duration-1000 group-hover:opacity-100"></div>
-                            {/* Contenido del botón */}
-                            <span className="relative z-10">Guardar Cambios</span>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+    console.log(data)
+    if (isLoading) return 'Cargando...'
+    if (isError) return <Navigate to='/404' />
+    if (data) return <AdminEditInsuranceForm data={data} />
 }
