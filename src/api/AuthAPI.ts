8acf947485b8eff7566ca1442b11a9c7b635b128
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-05-18 19:57:29 -0600

    Agregacion de funcion para consumir api login

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
+import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 import api from "../lib/axios";
 
 
@@ -32,6 +32,18 @@ export async function requestConfirmationCode(formData: RequestConfirmationCodeF
         const url = '/auth/request-code';
         const { data } = await api.post<string>(url, formData);
         return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+    try {
+        const url = '/auth/login';
+        const { data } = await api.post<string>(url, formData)
+        localStorage.setItem('AUTH_TOKEN', data);
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
