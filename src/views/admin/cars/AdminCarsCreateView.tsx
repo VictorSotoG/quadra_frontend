b841c40dd4a7811e0e8ce2e4f5230a2ab79c6611
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-05-19 22:42:02 -0600

    Cambios en vista para agregar un vehiculo

--- src/views/admin/cars/AdminCarsCreateView.tsx
+++ src/views/admin/cars/AdminCarsCreateView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import ErrorMessage from '../../../components/ErrorMessage'
 import { useForm } from 'react-hook-form'
 
@@ -236,6 +236,31 @@ export default function AdminCarsCreateView() {
                             />
                             {errors.precio_por_dia && (
                                 <ErrorMessage>{errors.precio_por_dia.message}</ErrorMessage>
+                            )}
+                        </div>
+
+                        
+
+                        <div className="mb-4">
+                            <label 
+                                className="block text-gray-700 mb-2" 
+                                htmlFor="tipo"
+                            >Estado del Auto</label>
+
+                            <select
+                                id="estado"
+                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
+                                {...register("tipo", {
+                                    required: "El tipo de auto es obligatorio"
+                                })}
+                            >
+                                <option value="">-- Seleccionar estado del vehiculo --</option>
+                                <option value="">Disponible</option>
+                                <option value="">En espera</option>
+                                <option value="">No disponible</option>
+                            </select>
+                            {errors.tipo && (
+                                <ErrorMessage>{errors.tipo.message}</ErrorMessage>
                             )}
                         </div>
 
